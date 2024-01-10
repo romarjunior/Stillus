0e2c7636235a2a49ffecc44d1bf6175b81a1aff6
romarjunior <156210895+romarjunior@users.noreply.github.com>
2024-01-10 11:32:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav(){
+    document.getElementById("menu-mobile-myNav").style.width = '100%';
+}
+
+function closeNav(){
+    document.getElementById("menu-mobile-myNav").style.width = '0%';
+}
